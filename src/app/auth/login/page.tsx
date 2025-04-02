85e50ba8b0cb50e56a6e97b01420741065968b53
LuisFelipeMod <luisfelipetecno@gmail.com>
2025-04-01 22:03:27 -0300

    refactor: change import path

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,12 @@
-"use client";
 
-import { CardWithForm } from "@/components/card-with-form";
+import { CardWithForm } from "@/components/ui/card-with-form";
 
 export function Login() {
   return (
     <section className="flex justify-center items-center h-screen">
       <CardWithForm type="signIn" />
     </section>
+    
   );
 }
 
