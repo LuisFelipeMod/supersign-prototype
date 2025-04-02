5c57bf5618e32a4d55b7c3475bdabe57a7c1433a
LuisFelipeMod <luisfelipetecno@gmail.com>
2025-04-01 21:54:36 -0300

    feat: redirects in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <>
-    </>
-  );
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  } else {
+    redirect("/dashboard");
+  }
+
+  return <></>;
 }
